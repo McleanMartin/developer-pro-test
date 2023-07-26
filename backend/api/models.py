935728475879675ed32c57eab64b95dc9353df4f
McleanMartin <chikadayamartin@gmail.com>
2023-07-26 15:22:27 +0300

    proficency test

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,54 +1,58 @@
 from django.db import models
 
-# Create your models here.
+class Department(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=50)
 
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    auto_now=False, auto_now_add=False)
     class Meta:
-        verbose_name = _("Company")
-        verbose_name_plural = _("Companys")
+        verbose_name =("Department")
+        verbose_name_plural =("Departments")
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
-        return reverse("Company_detail", kwargs={"pk": self.pk})
+        return reverse("Department_detail", kwargs={"pk": self.pk})
+
+
+class Company(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    reg_number = models.CharField(max_length=50)
+    reg_date = models.DateField(auto_now=False)
+    address = models.CharField(max_length=250)
+    contact_person = models.CharField(max_length=50)
+    total_employees = models.IntegerField()
+    cell = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    departments = models.OneToOneField(Department, verbose_name=("departments"), on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = ("Company")
+        verbose_name_plural = ("Companies")
+
+    def __str__(self):
+        return self.name
+
+class Employee(models.Model):
+    role = (
+        ('manager','manager'),
+        ('accountant','accountant'),
+        ('security','security')
+        )
+    emp_id = models.CharField(max_length=50,unique=True)
+    department = models.ForeignKey(Department,on_delete=models.CASCADE)
+    role = models.CharField(choices=role, max_length=50)
+
+    class Meta:
+        verbose_name =("Employee")
+        verbose_name_plural = ("Employees")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Employee_detail", kwargs={"pk": self.pk})
+
+
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api'# our backend api app
+    'api',# our backend api app
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
